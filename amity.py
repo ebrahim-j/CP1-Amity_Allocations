d2b35a82dd9bfd41d5fd09ad4b44449b57b0133c
ebrahim.j <ebrahim.janoowalla@andela.com>
2017-04-14 15:53:40 +0300

    Update amity.py

--- amity.py
+++ amity.py
@@ -2,7 +2,7 @@
 from person import Fellow, Staff
 from rooms import Office, LivingSpace, Room
 from random import randint, choice
-
+#correct for .lower() or likewise entries
 class Amity(object):
     """ class Amity """
     def __init__(self):
@@ -21,7 +21,7 @@ class Amity(object):
         selected_room = choice(rooms_with_space)
         return selected_room
 
-    
+
     def randomly_allocate_ls(self):
         rooms_with_space = []
         for room in self.l_spaces:
@@ -32,7 +32,7 @@ class Amity(object):
         selected_room = choice(rooms_with_space)
         return selected_room
 
-    
+
     def add_person(self, name, role, wants_accommodation='N'):
         # if not name.isalpha() or not role.isalpha() or not wants_accommodation.isalpha():
         #     return "Please ensure you don't have digits in name, role or accommodation"
@@ -56,25 +56,25 @@ class Amity(object):
                 self.fellows.append(person)
                 office_selected = Amity.randomly_allocate_office(self)
                 if office_selected == False:
-                    return "Welcome %s, You will be allocated an office as soon as we have space" %person.the_name 
+                    return "Welcome (%s) %s, You will be allocated an office as soon as we have space" %(person.the_id, person.the_name )
                 office_selected.current_occupants.append(person.the_name)
                 person.allocated = office_selected.room_name
-                print ( "(%s): %s has been allocated to %s" % (person.the_id, person.the_name, office_selected.room_name))
+                print ("(%s): %s has been allocated to %s" % (person.the_id, person.the_name, office_selected.room_name))
             elif wants_accommodation.upper() == "Y" or wants_accommodation.upper() == "YES":
                 person = Fellow(identifier, name)
                 self.fellows.append(person)
                 office_selected = Amity.randomly_allocate_office(self)
                 ls_selected = Amity.randomly_allocate_ls(self)
-                if office_selected == False and ls_selected == False: #fix this
-                    print("Welcome %s, You will be allocated an office and a Living Space as soon as we have space" %person.the_name)
+                if office_selected == False and ls_selected == False:
+                    print("Welcome (%s) %s, You will be allocated an office and a Living Space as soon as we have space" % (person.the_id,person.the_name))
                 elif office_selected == False and ls_selected != False:
                     ls_selected.current_occupants.append(person.the_name)
                     person.accommodated = ls_selected.room_name
-                    print("Welcome %s, You will live in %s and will be allocated an office as soon as we have space" %(person.the_name, ls_selected.room_name))
+                    print("Welcome (%s) %s, You will live in %s and will be allocated an office as soon as we have space" %(person.the_id, person.the_name, ls_selected.room_name))
                 elif ls_selected == False and office_selected != False:
                     office_selected.current_occupants.append(person.the_name)
                     person.allocated = office_selected.room_name
-                    print("Welcome %s, You have been allocated to %s. You will be assigned a living space as soon as we have room" %(person.the_name, office_selected.room_name))
+                    print("Welcome (%s) %s, You have been allocated to %s. You will be assigned a living space as soon as we have room" %(person.the_id, person.the_name, office_selected.room_name))
                 else:
                     office_selected.current_occupants.append(person.the_name)
                     person.allocated = office_selected.room_name
@@ -85,7 +85,7 @@ class Amity(object):
                 print("I don't know whether you want accomodation or not. (Reply with Y or Yes, N or No) ")
         else:
             print("Person can either be Staff or Fellow")
-            
+
 
 
 
@@ -93,6 +93,8 @@ class Amity(object):
         """instantiates a living space or office based on prefix"""
         #check for edge cases here    
         #query the db to see if room already exists
+        if not prefix.isalpha():
+            return "Room type cannot be in digits"
         if prefix.lower() == "office" or prefix.lower() == "o": #try regex
             room = Office(name)
             self.offices.append(room)
@@ -113,31 +115,32 @@ class Amity(object):
         for individual in employees:
             if individual.the_id == name_id:
                 if new_room_name.lower() in [room.room_name.lower() for room in self.offices]: #check for when person is alloccated to same room
-                    #what if staff is tried to be allocated to living space?
-                    #remove from one room and add to another's current occupants
+                    #add to another's current occupants
                     for room in self.offices:
                         if individual.the_name in room.current_occupants:
                             room.current_occupants.remove(individual.the_name)
                     for room in self.offices:
-                        if room.room_name == new_room_name:
+                        if room.room_name == new_room_name and room.room_has_space():
                             room.current_occupants.append(individual.the_name)
-                    individual.allocated = new_room_name
+                            individual.allocated = new_room_name
                     return "%s has been reallocated to %s" % (individual.the_name, new_room_name)
-                elif new_room_name.lower() in [room.room_name.lower() for room in self.l_spaces]:
+                elif not isinstance(individual, Staff) and new_room_name.lower() in [room.room_name.lower() for room in self.l_spaces]:
                     for room in self.l_spaces:
                         if individual.the_name in room.current_occupants:
                             room.current_occupants.remove(individual.the_name)
                     for room in self.l_spaces:
-                        if room.room_name == new_room_name:
+                        if room.room_name == new_room_name and room.room_has_space():
                             room.current_occupants.append(individual.the_name)
-                    individual.accommodated = new_room_name
+                            individual.accommodated = new_room_name
                     return "%s has been reallocated to %s" % (individual.the_name, new_room_name)
+                elif isinstance(individual, Staff) and new_room_name.lower() in [room.room_name.lower() for room in self.l_spaces]:
+                    return "Cannot allocate staff to a living Space"
                 else:
                     return "This room does not exist. (Make sure you spell check your room names)"
             else:
                 return "This person cannot be identified"
 
-    def load_people(self, fileobj): 
+    def load_people(self, fileobj):
         """ Adds people to rooms from a txt file """
         #try except this to see if file exists
         # try:
@@ -147,9 +150,7 @@ class Amity(object):
         with open(fileobj, 'r') as my_file:
             info = my_file.readlines()
             for argument in info:
-                #print(argument)
                 argument = argument.split()
-                #print(argument)
                 name = str(argument[0]) + " " + str(argument[1])
                 role = str(argument[2].strip())
                 if len(argument) == 3:
@@ -159,9 +160,9 @@ class Amity(object):
                     wants_accommodation = str(argument[3])
                     print(self.add_person(name, role, wants_accommodation))      
                 # return "Information provided not complete"
-                
 
-        
+
+
         # except:
         #     return "File does not exist"
 
@@ -169,7 +170,7 @@ class Amity(object):
     def print_allocations(self):
         """ Prints a list of allocations.
         Specifying the optional -o option outputs the registered allocations to a txt file """
-        
+
         if len(self.offices) == 0:
             print("NO offices added yet!")
         else:
@@ -210,15 +211,17 @@ class Amity(object):
 
         for person in unallocated:
             print("%s:- %s" % (person[0], person[1]))
-            
 
     def print_room(self, room_name):
         """ Prints the names of all the people in room_name on the screen """
         all_rooms = self.offices + self.l_spaces
-        for one_room in all_rooms:
-            if room_name in [room.room_name for room in all_rooms] and room_name.lower() == one_room.room_name.lower():
-                print ("Occcupants in %s:" % one_room.room_name.upper())
-                print (", ".join(one_room.current_occupants))
+        try:
+            for one_room in all_rooms:
+                if room_name in [room.room_name for room in all_rooms] and room_name.lower() == one_room.room_name.lower():
+                    print ("Occcupants in %s:" % one_room.room_name.upper())
+                    print (", ".join(one_room.current_occupants))
+        except:
+            return "Room not found"
 
 
     def save_state(self):
@@ -235,16 +238,23 @@ amity = Amity()
 
 amity.create_room("o", "oculus")
 amity.create_room("l", "Homabay")
-amity.add_person("Faith", "fellow")
-# amity.add_person("Mulobi", "fellow", "Yes")
+amity.create_room("l", "Surat")
+amity.create_room("o", "Rome")
+# amity.add_person("Faith", "fellow")
+amity.add_person("Mulobi", "fellow", "Yes")
 # amity.add_person("Alex", "staff", "Y")
 # amity.add_person("Paul", "fellow", "Y")
 # amity.add_person("Millicent", "staff", "N")
 # amity.add_person("Ahmed", "fellow", "N")
-print(amity.load_people("text.txt"))
-amity.print_allocations()
-# amity.print_room("oculus")
+# print(amity.load_people("text.txt"))
+# amity.print_allocations()
+amity.print_room("Surat")
+amity.print_room("Homabay")
+
 print("")
 amity.print_unallocated()
-amity.reallocate_person(1, "Homabay")
+print(amity.reallocate_person(11, "Surat"))
+
+amity.print_room("Surat")
+amity.print_room("Homabay")
 # import pdb; pdb.set_trace()
