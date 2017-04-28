2e9a02837fc17dfaaa3bad2aef710a603c74f041
ebrahim.j <ebrahim.janoowalla@andela.com>
2017-04-28 10:14:46 +0300

    pep8 corrections

--- models/dbModels.py
+++ models/dbModels.py
@@ -8,18 +8,19 @@ Base = declarative_base()
 
 
 class PersonModel(Base):
-	__tablename__ = 'person'
-	person_id = Column(Integer, primary_key=True)
-	name = Column(String(200), nullable=False)
-	role = Column(String(50))
-	office_space = Column(String(50), ForeignKey('room.name'))
-	living_space = Column(String(50), ForeignKey('room.name'))
+    """ Person model table for People in Amity"""
+    __tablename__ = 'person'
+    person_id = Column(Integer, primary_key=True)
+    name = Column(String(200), nullable=False)
+    role = Column(String(50))
+    office_space = Column(String(50), ForeignKey('room.name'))
+    living_space = Column(String(50), ForeignKey('room.name'))
 
 class RoomModel(Base):
-	__tablename__ = 'room'
-	id = Column(Integer, primary_key=True)
-	name = Column(String(200))
-	room_type = Column(String(50))
-	people = relationship("PersonModel", foreign_keys=[PersonModel.office_space])
-	people = relationship("PersonModel", foreign_keys=[PersonModel.living_space])
-
+    """ Room model table for Rooms in Amity"""
+    __tablename__ = 'room'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(200))
+    room_type = Column(String(50))
+    people = relationship("PersonModel", foreign_keys=[PersonModel.office_space])
+    people = relationship("PersonModel", foreign_keys=[PersonModel.living_space])
